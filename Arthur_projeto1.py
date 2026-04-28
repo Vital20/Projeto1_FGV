23a23e126e6c47860a0c2230c8c18b2da5f1bde1
Arthur Vital <149189944+Vital20@users.noreply.github.com>
2026-04-28 15:26:37 -0300

    Update Arthur_projeto1.py

--- Arthur_projeto1.py
+++ Arthur_projeto1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title('Botafogo e sua relação com as Copas da Seleção brasileira')
-st.subheader('Por: Arthur Vital
+st.subheader('Por: Arthur Vital')
 st.write("O Botafogo é um dos clube mais tradicionais do brasil, sendo responsável diretamente por 3 das 5 Copas do Mundo do Brasil, mas entenda quando e onde tudo isso começou...")
 
 st.image('https://s2-oglobo.glbimg.com/5mQLlKwt25ljz6ZCj0RXzCuOBzI=/0x0:2000x1194/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2024/q/e/zhOyUtR5Kw92vopn1zAw/arte-40-.png' , caption='Jairzinho, Luiz Henrique e Igor Jesus')
