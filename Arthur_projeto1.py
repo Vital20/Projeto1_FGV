87d04ba8618afe9c921633c80199945efeb70433
Arthur Vital <149189944+Vital20@users.noreply.github.com>
2026-04-28 15:13:33 -0300

    Update Arthur_projeto1.py

--- Arthur_projeto1.py
+++ Arthur_projeto1.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
-st.title('Teste ECMI 2')
-st.write("Botafogo, Botafogo, Campeão, desde 1907! Foste herói em cada ")
+st.title('Botafogo e sua relação com as copas')
+st.write("O botafogo é o clube mais tradicional do brasil, sendo responsável diretamente por 3 das 5 Copas do Mundo do Brasil.")
+
+st.image(https://s2-oglobo.glbimg.com/5mQLlKwt25ljz6ZCj0RXzCuOBzI=/0x0:2000x1194/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2024/q/e/zhOyUtR5Kw92vopn1zAw/arte-40-.png, caption='Processada com PIL')
