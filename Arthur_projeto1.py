5dc5ac6d8c255b418b55fd4c775ca092c09ec019
Arthur Vital <149189944+Vital20@users.noreply.github.com>
2026-05-01 10:00:06 -0300

    Update Arthur_projeto1.py

--- Arthur_projeto1.py
+++ Arthur_projeto1.py
@@ -23,9 +23,17 @@ if copa == "1958":
     st.write("""
     A Copa de 1958 marca o início de uma hegemonia do futebol brasileiro no mundo, e o Botafogo teve papel crucial nisso.
     
-    Jogadores como Garrincha e Nilton Santos foram fundamentais na campanha, trazendo um estilo de jogo ofensivo,
+    Impossível não destacar Garrincha e Nilton Santos, eles foram fundamentais na campanha, trazendo um estilo de jogo ofensivo,
     técnico e criativo que encantou o mundo.
     
     Garrincha, principalmente, foi decisivo nos momentos mais importantes, sendo considerado um dos grandes nomes
     daquela conquista. Já Nilton Santos criou a posição "lateral", apoiando o ataque de forma inédita.
     """)
+    # 👇 AQUI entram as imagens depois do texto
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.image("https://p2.trrsf.com/image/fget/cf/1200/1600/middle/images.terra.com/2022/12/01/1261386654-garrincha-drible.jpg", caption="Garrincha", use_container_width=True)
+
+    with col2:
+        st.image("https://terceirotempo.uol.com.br/imagens/31/71/w500_h140_qfl_fto_13171.webp", caption="Nilton Santos", use_container_width=True)
