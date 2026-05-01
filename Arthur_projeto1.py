6f9e1a117acb88a7554b6838b178801858bf8148
Arthur Vital <149189944+Vital20@users.noreply.github.com>
2026-04-30 23:51:57 -0300

    Update Arthur_projeto1.py

--- Arthur_projeto1.py
+++ Arthur_projeto1.py
@@ -1,10 +1,15 @@
 import streamlit as st
 
-st.title('Botafogo e sua relação com as Copas da Seleção brasileira')
+st.title('Botafogo em Copas do Mundo da Seleção brasileira')
 st.subheader('Por: Arthur Vital')
-st.write("O Botafogo é um dos clube mais tradicionais do brasil, sendo responsável diretamente por 3 das 5 Copas do Mundo do Brasil, mas entenda quando e onde tudo isso começou...")
+st.write("""
+O Botafogo é um dos clubes mais tradicionais do Brasil, sendo peça fundamental
+em momentos históricos da Seleção Brasileira, especialmente nas Copas do Mundo.
+Mas como essa relação começou? E por que o clube é tão importante nesse contexto?
+""")
 
 st.image('https://s2-oglobo.glbimg.com/5mQLlKwt25ljz6ZCj0RXzCuOBzI=/0x0:2000x1194/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2024/q/e/zhOyUtR5Kw92vopn1zAw/arte-40-.png' , caption='Jairzinho, Luiz Henrique e Igor Jesus')
+st.divider()
 
-nome= st.text_input('Fale o maior jogador do Botafogo que passou pela seleção:')
-
+# 📜 Seção histórica interativa
+st.header("📜 Momentos históricos")
