3e9022b6c922133c152326acdc530852d0c7c9c8
Arthur Vital <149189944+Vital20@users.noreply.github.com>
2026-05-05 14:51:48 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -5,4 +5,8 @@ df= pd.read_csv('deputados_2022.csv')
 st.dataframe(df)
 
 st.title('Buscador de deputados por partido:')
-st.text_input('digite a sigla do partido: ')
+partido = st.text_input('Digite a sigla do partido: ')
+
+if partido:
+    filtrado = df[df['partido'].str.upper() == partido.upper()]
+    st.dataframe(filtrado)
