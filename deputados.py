b8742353a9a3d2516afaa2ab9f510f4d58c29731
Arthur Vital <149189944+Vital20@users.noreply.github.com>
2026-05-05 14:57:20 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -4,7 +4,7 @@ import pandas as pd
 df= pd.read_csv('deputados_2022.csv')
 
 st.title('Buscador de deputados por partido:')
-st.text_input('digite a sigla do partido: ')
+st.text_input('Digite a sigla do partido: ')
 partido = st.text_input('Digite a sigla do partido: ')
 
 if partido:
