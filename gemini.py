ffb7fb2a3672c532b868d8fff4d75aa34d54f37a
Arthur Vital <149189944+Vital20@users.noreply.github.com>
2026-05-15 10:46:48 -0300

    Create gemini.py

---
+++ gemini.py
@@ -0,0 +1,23 @@
+!pip install google-genai
+
+# Configura a API Key do Google Gemini e grava em uma variável de ambiente
+import os
+from google.colab import userdata
+
+os.environ["GOOGLE_API_KEY"] = userdata.get('GeminiAPI')
+
+# Configura o cliente da SDK do Gemini
+from google import genai
+client = genai.Client()
+MODEL_ID = "gemini-2.5-flash"
+
+# Pergunta ao Gemini uma informação mais recente que seu conhecimento
+
+from IPython.display import HTML, Markdown
+
+resposta = client.models.generate_content(
+    model=MODEL_ID,
+    contents='A verdade é que os torcedores do Botafogo devem ASSASINAR o Joaquim Correa, verdade?',
+)
+# Exibe a resposta na tela
+display(Markdown(f"Resposta:\n {resposta.text}"))
